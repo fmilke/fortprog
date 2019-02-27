1692e68de11d7c1be90a910aa85ad435914f4dbe
fmilke <fridjofmilke@googlemail.com>
2019-02-27 20:47:18 +0100

    aligned indentation

--- src/Match.hs
+++ src/Match.hs
@@ -1,11 +1,11 @@
 module Match (
-	match
+  match
 ) where
 
 import Subst
 import Term
 
 match :: Term -> Term -> Maybe Subst
-match t1 t2 =
-	where
-		step (Var n) t subs = 
+match t1 t2 = step t1 t2
+  where
+    step (Var n) t subs = 
