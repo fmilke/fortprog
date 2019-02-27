cc5631a6b83a218a1fe7cf1c7f3873cb468f4054
fmilke <fridjofmilke@googlemail.com>
2019-02-27 19:42:05 +0100

    polished pretty (no obsolete spaces)

--- src/Pretty.hs
+++ src/Pretty.hs
@@ -1,16 +1,16 @@
 module Pretty(
-	Pretty
+  Pretty
 ) where
 
+import Data.List(intercalate)
 import Term
 
 -- Class instance for
 -- displaying anything in pretty
 class Pretty a where
-	pretty :: a -> String
-
+  pretty :: a -> String
 
 instance Pretty (Term) where
-	pretty (Var n) = n
-	pretty (Comb n []) = n
-	pretty (Comb n ts) = n ++ " (" ++ (foldr (\t rest -> (pretty t) ++ " " ++ rest) "" ts) ++ ")"
+  pretty (Var n) = n
+  pretty (Comb n []) = n
+  pretty (Comb n ts) = n ++ " (" ++ (intercalate " " (map (\t -> pretty t) ts)) ++ ")"
