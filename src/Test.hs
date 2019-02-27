8094753ef5600b5626fd37e1b358bb63ac8fa4b9
fmilke <fridjofmilke@googlemail.com>
2019-02-27 19:51:52 +0100

    refined automated testing

--- src/Test.hs
+++ src/Test.hs
@@ -1,14 +1,22 @@
 import Term
 import Pos
 
--- tests
+testTargets = [
+    testReplace,
+    testReplace2,
+    testAllPos1
+  ]
+
+testAll :: IO ()
+testAll = doTest testTargets
+  where
+    doTest []     = putStrLn("All tests passed!")
+    doTest (t:ts) = if t == True then doTest ts else putStrLn "Test failed"
+
 testEntity = Comb "+" [Var "x", Var "y"]
 testEntity2 = Comb "+" [Comb "*" [Var "x", Var "z"], Var "y"]
 
---  should be Comb "+" [Var "z", Var "y"]
-testReplace = (replaceAt testEntity [0] (Var "z"))
---  should be Comb "+" [Comb "*" [Var "x", Var "Zzz"], Var "y"]
-testReplace2 = (replaceAt testEntity2 [0, 1] (Var "Zzz"))
+testReplace = (replaceAt testEntity [0] (Var "z")) == Comb "+" [Var "z", Var "y"]
+testReplace2 = (replaceAt testEntity2 [0, 1] (Var "Zzz")) == Comb "+" [Comb "*" [Var "x", Var "Zzz"], Var "y"]
 
--- should be [[0,0],[0,1],[1]]
-testAllPos1 = allPos testEntity2
+testAllPos1 = (allPos testEntity2) == [[0,0],[0,1],[1]]
