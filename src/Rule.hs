1808b454bc7db8653b79b1b94e37ffa49e4fb7b0
fmilke <fridjofmilke@googlemail.com>
2019-02-28 16:52:29 +0100

    fixed comments, renamed scoped func

--- src/Rule.hs
+++ src/Rule.hs
@@ -14,18 +14,18 @@ findRule (Prog ((Rule lhs rhs):rs)) t = case match t lhs of
   Nothing    -> Nothing
   Just subst -> Just (rhs, subst)
 
--- returns Nothing
--- if not able to reduce at the given position
+-- returns Nothing if not able
+-- to reduce at the given position
 reduceAt :: Prog -> Term -> Pos -> Maybe Term
 reduceAt prog t pos = let subTerm = selectAt t pos in
   case findRule prog subTerm of
     Nothing           -> Nothing
     Just (rhs, subst) -> Just (replaceAt t pos rhs)
 
--- reduciblePos :: Prog -> Term -> [Pos]
-reduciblePos prog t = filter predicate (allPos t)
+reduciblePos :: Prog -> Term -> [Pos]
+reduciblePos prog t = filter reducible (allPos t)
   where
-    predicate pos = case reduceAt prog t pos of
+    reducible pos = case reduceAt prog t pos of
       Nothing -> False
       Just _  -> True
 
