3e5dadd6049614e16079d3bbc4c462cfa765ed2b
fmilke <fridjofmilke@googlemail.com>
2019-02-28 13:44:26 +0100

    added Rule.hs and its function signatures

---
+++ src/Rule.hs
@@ -0,0 +1,9 @@
+module Rule where
+
+import Pos
+import Match
+
+-- findRule :: Prog -> Term -> Maybe (Rhs, Subst)
+-- reduceAt :: Prog -> Term -> Pos -> Maybe Term
+-- reduciblePos :: Prog -> Term -> [Pos]
+-- isNormalForm :: Prog -> Term -> Bool
