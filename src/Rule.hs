a03bed726557f4d09c0b1f0169350a5e236f1ffc
fmilke <fridjofmilke@googlemail.com>
2019-02-28 14:47:45 +0100

    implenteds functions for rules

--- src/Rule.hs
+++ src/Rule.hs
@@ -1,9 +1,35 @@
 module Rule where
 
+import Data.Maybe(isNothing)
 import Pos
 import Match
+import Prog
+import Subst
+import Term
 
--- findRule :: Prog -> Term -> Maybe (Rhs, Subst)
--- reduceAt :: Prog -> Term -> Pos -> Maybe Term
+-- does stuff
+findRule :: Prog -> Term -> Maybe (Rhs, Subst)
+findRule (Prog [])     _  = Nothing
+findRule (Prog ((Rule lhs rhs):rs)) t = case match t lhs of
+  Nothing    -> Nothing
+  Just subst -> Just (rhs, subst)
+
+-- returns Nothing
+-- if not able to reduce at the given position
+reduceAt :: Prog -> Term -> Pos -> Maybe Term
+reduceAt prog t pos = let subTerm = selectAt t pos in
+  case findRule prog subTerm of
+    Nothing           -> Nothing
+    Just (rhs, subst) -> Just (replaceAt t pos rhs)
+
 -- reduciblePos :: Prog -> Term -> [Pos]
--- isNormalForm :: Prog -> Term -> Bool
+reduciblePos prog t = filter predicate (allPos t)
+  where
+    predicate pos = case reduceAt prog t pos of
+      Nothing -> False
+      Just _  -> True
+
+-- we call a term of normal form
+-- if there is no rule left matching it
+isNormalForm :: Prog -> Term -> Bool
+isNormalForm prog t = isNothing (findRule prog t)
