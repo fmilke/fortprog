b8530d5f330898e2da9de9ac5ec11cf46cbd6d38
fmilke <fridjofmilke@googlemail.com>
2019-03-02 10:13:50 +0100

    removed unused vars

--- src/Utils.hs
+++ src/Utils.hs
@@ -3,5 +3,5 @@ module Utils (mapNth) where
 -- map like function, but only applies to the nth element
 mapNth :: (a -> a) -> [a] -> Int -> [a]
 mapNth fn (a:as) 0 = (fn a) : as
-mapNth fn []     n = error ("Access out of bound index")
+mapNth _  []     _ = error ("Access out of bound index")
 mapNth fn (a:as) n = a : (mapNth fn as (n - 1))
