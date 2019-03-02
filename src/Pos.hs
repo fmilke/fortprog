1094a14fd026439bb51f6bf5248036c3200e6caa
fmilke <fridjofmilke@googlemail.com>
2019-03-02 10:17:53 +0100

    fixed indentation

--- src/Pos.hs
+++ src/Pos.hs
@@ -17,25 +17,25 @@ import Utils(mapNth)
 -- Pos(y) = 1 : []
 type Pos = [Int]
 
-  above :: Pos -> Pos -> Bool
-  above [] _ = True
-  above _ [] = False
-  above (p:ps) (q:qs) = p == q && above ps qs
-      
-  below :: Pos -> Pos -> Bool
-  below [] _ = False
-  below _ [] = True
-  below (p:ps) (q:qs) = p == q && below ps qs
-  
-  leftOf :: Pos -> Pos -> Bool
-  leftOf [] _ = False
-  leftOf _ [] = False
-  leftOf (p:ps) (q:qs) = p < q || (p == q && leftOf ps qs)
+above :: Pos -> Pos -> Bool
+above [] _ = True
+above _ [] = False
+above (p:ps) (q:qs) = p == q && above ps qs
     
-  rightOf :: Pos -> Pos -> Bool
-  rightOf [] _ = False
-  rightOf _ [] = False
-  rightOf (p:ps) (q:qs) = p > q || (p == q && rightOf ps qs)
+below :: Pos -> Pos -> Bool
+below [] _ = False
+below _ [] = True
+below (p:ps) (q:qs) = p == q && below ps qs
+
+leftOf :: Pos -> Pos -> Bool
+leftOf [] _ = False
+leftOf _ [] = False
+leftOf (p:ps) (q:qs) = p < q || (p == q && leftOf ps qs)
+  
+rightOf :: Pos -> Pos -> Bool
+rightOf [] _ = False
+rightOf _ [] = False
+rightOf (p:ps) (q:qs) = p > q || (p == q && rightOf ps qs)
 
 -- selects the sub-term at the given position
 selectAt :: Term -> Pos -> Term
@@ -57,7 +57,7 @@ replaceAt (Comb n ts) (p:ps) t2 = Comb n (
 -- returns all possible position of sub-terms in a given term
 allPos :: Term -> [Pos]
 allPos (Var _)  = [[]]
-allPos (Comb _ ts) = iter ts 0
+allPos (Comb _ ts) = [] : iter ts 1
   where
-    iter []     n = []
+    iter []     _ = []
     iter (t:ts) n = (map (\p -> n : p) (allPos t)) ++ (iter ts (n + 1))
