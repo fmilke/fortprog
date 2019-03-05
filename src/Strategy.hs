60968b879a7e5aebec9fd297b2d2d8c6d8fd32f4
fmilke <fridjofmilke@googlemail.com>
2019-03-05 19:46:54 +0100

    implemented test strategies

--- src/Strategy.hs
+++ src/Strategy.hs
@@ -1,13 +1,51 @@
+module Strategy where
+
+import Pos
+import Rule
+import Term
+import Prog
+
+import Data.List(sortBy)
+
 -- Alias type for evaluation strategies.
 type Strategy = Prog -> Term -> [Pos]
 
--- loStrategy :: Strategy
--- liStrategy :: Strategy
--- roStrategy :: Strategy
--- riStrategy :: Strategy
+loSort :: Pos -> Pos -> Ordering
+loSort a b | a `above` b  = LT
+           | a `leftOf` b = LT
+           | otherwise    = GT
+
+loStrategy :: Strategy
+loStrategy prog term = sortBy loSort (reduciblePos prog term)
+
+liSort :: Pos -> Pos -> Ordering
+liSort a b | a `below` b  = LT
+           | a `leftOf` b = LT
+           | otherwise    = GT
+
+liStrategy :: Strategy
+liStrategy prog term = sortBy liSort (reduciblePos prog term)
+
+roSort :: Pos -> Pos -> Ordering
+roSort a b | a `above` b  = LT
+           | a `rightOf` b = LT
+           | otherwise    = GT
+
+roStrategy :: Strategy
+roStrategy prog term = sortBy roSort (reduciblePos prog term)
+
+riSort :: Pos -> Pos -> Ordering
+riSort a b | a `below` b  = LT
+           | a `rightOf` b = LT
+           | otherwise    = GT
+
+riStrategy :: Strategy
+riStrategy prog term = sortBy riSort (reduciblePos prog term)
 
 -- poStrategy :: Strategy
 -- piStrategy :: Strategy
 
 -- reduceWith :: Strategy -> Prog -> Term -> Maybe Term
+
 -- evaluateWith :: Strategy -> Prog -> Term -> Term
+-- evaluateWith strat prog t = strat prog t
