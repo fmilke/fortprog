8dc344c33c54062dbfc56ddb89b1623ba49d6cd0
fmilke <fridjofmilke@googlemail.com>
2019-03-05 20:54:27 +0100

    added basic cli

---
+++ src/Cli.hs
@@ -0,0 +1,38 @@
+
+helpMsg :: String
+helpMsg = "Commands available from the prompt:\n\
+  \  <expression>       Evaluates the specified expression.\n\
+  \  :h[elp]            Shows this help message.\n\
+  \  :l[oad] <file>     Loads the specified file.\n\
+  \  :l[oad]            Unloads the currently loaded file.\n\
+  \  :r[eload]          Reloads the lastly loaded file.\n\
+  \  :s[et] <strategy>  Sets the specified evaluation strategy\n\
+  \                     where <strategy> is one of 'lo', 'li',\n\
+  \                     'ro', 'ri', 'po', or 'pi'.\n\
+  \  :q[uit]            Exits the interactive environment."
+
+invalidInputMsg :: String
+invalidInputMsg = "Invalid input. Try :help for a list of commands"
+
+quitMsg :: String
+quitMsg = "Leaving..."
+
+greeting :: String
+greeting = "Welcome to Simple Haskell! \n\
+  \Type \":help\" for help."
+
+main :: IO ()
+main = putStrLn greeting >> inputLoop
+
+inputLoop :: IO ()
+inputLoop = do
+  inp <- getLine
+  case handleInput inp of
+    Just io -> io >> inputLoop
+    Nothing -> putStrLn quitMsg
+
+handleInput :: String -> Maybe (IO ())
+handleInput inp
+  | inp `elem` [":h", ":help"] = Just (putStrLn helpMsg)
+  | inp `elem` [":q", ":quit"] = Nothing
+  | otherwise                  = Just (putStrLn invalidInputMsg)
