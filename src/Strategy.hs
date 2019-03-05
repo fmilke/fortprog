75a8538be0c6295fe4a2d6d52127a7db84e44cfa
fmilke <fridjofmilke@googlemail.com>
2019-03-05 20:29:23 +0100

    made strategy comparisons local to function

--- src/Strategy.hs
+++ src/Strategy.hs
@@ -7,40 +7,36 @@ import Prog
 
 import Data.List(sortBy)
 
--- Alias type for evaluation strategies.
+-- Alias type for evaluation strategies
 type Strategy = Prog -> Term -> [Pos]
 
-loSort :: Pos -> Pos -> Ordering
-loSort a b | a `above` b  = LT
-           | a `leftOf` b = LT
-           | otherwise    = GT
-
 loStrategy :: Strategy
-loStrategy prog term = sortBy loSort (reduciblePos prog term)
-
-liSort :: Pos -> Pos -> Ordering
-liSort a b | a `below` b  = LT
-           | a `leftOf` b = LT
-           | otherwise    = GT
+loStrategy prog term = sortBy comp (reduciblePos prog term)
+  where
+    comp a b | a `above` b  = LT
+             | a `leftOf` b = LT
+             | otherwise    = GT
 
 liStrategy :: Strategy
-liStrategy prog term = sortBy liSort (reduciblePos prog term)
-
-roSort :: Pos -> Pos -> Ordering
-roSort a b | a `above` b  = LT
-           | a `rightOf` b = LT
-           | otherwise    = GT
+liStrategy prog term = sortBy comp (reduciblePos prog term)
+  where
+    comp a b | a `below` b  = LT
+             | a `leftOf` b = LT
+             | otherwise    = GT
 
 roStrategy :: Strategy
-roStrategy prog term = sortBy roSort (reduciblePos prog term)
-
-riSort :: Pos -> Pos -> Ordering
-riSort a b | a `below` b  = LT
-           | a `rightOf` b = LT
-           | otherwise    = GT
+roStrategy prog term = sortBy comp (reduciblePos prog term)
+  where
+    comp a b | a `above` b   = LT
+             | a `rightOf` b = LT
+             | otherwise     = GT
 
 riStrategy :: Strategy
-riStrategy prog term = sortBy riSort (reduciblePos prog term)
+riStrategy prog term = sortBy comp (reduciblePos prog term)
+  where
+    comp a b | a `below` b   = LT
+             | a `rightOf` b = LT
+             | otherwise     = GT
 
 -- poStrategy :: Strategy
 -- piStrategy :: Strategy
