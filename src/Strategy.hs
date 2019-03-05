ae398b8a4aeea2481c0aa64ab43a09a19b7cf72d
fmilke <fridjofmilke@googlemail.com>
2019-03-05 13:34:37 +0100

    added stub for ex. 6

---
+++ src/Strategy.hs
@@ -0,0 +1,13 @@
+-- Alias type for evaluation strategies.
+type Strategy = Prog -> Term -> [Pos]
+
+-- loStrategy :: Strategy
+-- liStrategy :: Strategy
+-- roStrategy :: Strategy
+-- riStrategy :: Strategy
+
+-- poStrategy :: Strategy
+-- piStrategy :: Strategy
+
+-- reduceWith :: Strategy -> Prog -> Term -> Maybe Term
+-- evaluateWith :: Strategy -> Prog -> Term -> Term
