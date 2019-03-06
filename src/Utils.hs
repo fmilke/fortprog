5859c268bb013bb80f98d48b8f6d11058e815121
fmilke <fridjofmilke@googlemail.com>
2019-03-06 21:11:29 +0100

    fixed a bug occuring after changing positions in a term to start at 1

--- src/Utils.hs
+++ src/Utils.hs
@@ -2,6 +2,6 @@ module Utils (mapNth) where
 
 -- map like function, but only applies to the nth element
 mapNth :: (a -> a) -> [a] -> Int -> [a]
-mapNth fn (a:as) 0 = (fn a) : as
+mapNth fn (a:as) 1 = (fn a) : as
 mapNth _  []     _ = error ("Access out of bound index")
 mapNth fn (a:as) n = a : (mapNth fn as (n - 1))
